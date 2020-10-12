a316c0b68d9a6697a4e349ee1e8cf7cd3597df21
pmqueiroz <pedroqueiroz593@gmail.com>
2020-10-12 11:09:41 -0300

    chore: landing page done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './assets/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Word</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+         <img src={logoImg} alt="Happy"/>
+
+         <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas criancas</p>
+         </main>
+
+         <div className="location">
+            <strong>Bahia</strong>
+            <span>Salvador</span>
+         </div>
+
+         <a href="#page-landing" className="enter-app">
+            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+         </a>
+      </div>
     </div>
   );
 }
